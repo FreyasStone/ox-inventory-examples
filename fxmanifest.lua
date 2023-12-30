892b282fa4a73e2ae49f828d82af2190a4cf5536
FreyasStone <155216445+FreyasStone@users.noreply.github.com>
2023-12-30 14:13:13 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+--[[ FX Information ]]--
+fx_version   'cerulean'
+game         'gta5'
+
+--[[ Resource Information ]]--
+name         'ox_test'
+author       'FretaTos'
+version      '1.0.0'
+repository   ''
+description  ''
+
+--[[ Manifest ]]--
+dependencies {
+    '/onesync',
+}
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
